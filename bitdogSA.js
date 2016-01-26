fee26d76e2ef2c5d74d6cab4c7c36ed6e67228c2
Vincent Miceli <vjm@orchardparksoftware.com>
2016-01-26 16:43:33 -0500

    Adding formalized objects for zwave node and value.

--- bitdogSA.js
+++ bitdogSA.js
@@ -1,1 +1,25 @@
-﻿var zwave = require('./lib/zwave.js');
+﻿var BitdogZWave = require('../lib/bitdogZWave.js');
+var bitdogClient = require('bitdog-client');
+var bitdogZWave = null;
+
+
+bitdogClient.on('ready', function () {
+
+    bitdogZWave = new BitdogZWave();
+    bitdogZWave.start();
+
+});
+
+process.on('SIGINT', function () {
+    bitdogClient.logger.logProcessEvent('disconnecting...');
+    
+    if (bitdogZWave != null)
+        bitdogZWave.stop();
+    
+    bitdogClient.stop();
+
+    process.exit();
+});
+
+
+bitdogClient.start();
