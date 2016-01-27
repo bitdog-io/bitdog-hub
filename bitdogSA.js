c7fe9925dde3439fc0fc3506d4446e9b978eb94b
Vincent Miceli <vjm@orchardparksoftware.com>
2016-01-26 21:31:23 -0500

    Updated logging.

--- bitdogSA.js
+++ bitdogSA.js
@@ -1,5 +1,6 @@
-﻿var BitdogZWave = require('../lib/bitdogZWave.js');
-var bitdogClient = require('bitdog-client');
+﻿var bitdogClient = require('bitdog-client');
+var constants = require('./lib/constants.js');
+var BitdogZWave = require('./lib/bitdogZWave.js');
 var bitdogZWave = null;
 
 
@@ -11,11 +12,14 @@ bitdogClient.on('ready', function () {
 });
 
 process.on('SIGINT', function () {
-    bitdogClient.logger.logProcessEvent('disconnecting...');
-    
-    if (bitdogZWave != null)
+    bitdogClient.logger.logProcessEvent(constants.LOG_PROCESS_BITDOG_SA, 'SIGINT, stopping.');
+
+    if (bitdogZWave != null) {
+        bitdogClient.logger.logProcessEvent(constants.LOG_PROCESS_ZWAVE, 'Stopping.');
         bitdogZWave.stop();
+    }
     
+    bitdogClient.logger.logProcessEvent(constants.LOG_PROCESS_BITDOG_CLIENT, 'Stopping.');
     bitdogClient.stop();
 
     process.exit();
