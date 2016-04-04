23912cc32c33e1bc25ab01e1f051cb9aea5f0e30
Vincent Miceli <vjm@orchardparksoftware.com>
2016-04-04 14:01:33 -0400

    Added command to example

--- examples/example.js
+++ examples/example.js
@@ -3,6 +3,13 @@ var bitdogClient = require('bitdog-client');
 
 bitdogSA.start();
 
+bitdogClient.addCommand('Turn local LED on/off', bitdogClient.commonMessageSchemas.onOffMessageSchema, function (message, configuration, logger) {
+    
+    // Every time this command is received, we will simply log the fact
+    logger.log('User', 'Turn local LED on/off' + message.value);
+
+});
+
 process.on('SIGINT', function () {
     bitdogClient.logger.logProcessEvent('Example Security and Automation', 'SIGINT, stopping.');
     bitdogSA.stop();
