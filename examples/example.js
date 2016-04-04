b1acae89c83a3d57f81b5569e436bc08635d4b6c
Vincent Miceli <vjm@orchardparksoftware.com>
2016-04-04 11:39:11 -0400

    Added example code and moving towards extensibility

---
+++ examples/example.js
@@ -0,0 +1,10 @@
+﻿var bitdogSA = require('../lib/bitdogSA.js');
+var bitdogClient = require('bitdog-client');
+
+bitdogSA.start();
+
+process.on('SIGINT', function () {
+    bitdogClient.logger.logProcessEvent('Example Security and Automation', 'SIGINT, stopping.');
+    bitdogSA.stop();
+    process.exit();
+});
