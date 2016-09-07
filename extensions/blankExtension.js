8045eb3e4dae61e765ece6a7ea20590bfb418ecc
Vincent Miceli <vjm@orchardparksoftware.com>
2016-09-07 11:29:09 -0400

    changed directory name for extensions

---
+++ extensions/blankExtension.js
@@ -0,0 +1,21 @@
+﻿// Extension will inherit functions from ExtensionBase
+// Make sure the path to extensionBase is correct 
+var ExtensionBase = require('../lib/extensionBase.js');
+var util = require('util');
+
+function Extension() {
+}
+// Extension inherits from ExtensionBase
+util.inherits(Extension, ExtensionBase);
+
+Extension.prototype.onMessage = function (message, configuration, logger) {
+};
+
+Extension.prototype.onInitialize = function (configuration, logger) {
+};
+
+// Export your Extension class so it can be loaded by the framework
+module.exports = Extension;
+
+
+
