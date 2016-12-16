3500ac5917d154c02bcb12084bb19c9ada783492
Vincent Miceli <vjm@orchardparksoftware.com>
2016-12-16 15:46:05 -0500

    bug

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="globals/commander/index.d.ts" />
+/// <reference path="globals/moment/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/rpio/index.d.ts" />
